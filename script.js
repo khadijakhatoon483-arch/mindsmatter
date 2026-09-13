c4ca3c396889bb4c60ee26c427a63872e0d76a00
khadijakhatoon483-arch <khadijakhatoon483@gmail.com>
2026-09-13 15:37:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Appointment button click handler
+document.getElementById('appointmentBtn').addEventListener('click', function() {
+  alert('Redirecting to appointment booking page...');
+  // You can replace alert with actual redirect:
+  // window.location.href = 'https://yourbookingpage.com';
+});
